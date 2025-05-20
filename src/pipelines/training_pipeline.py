de2da4bef682a0e7ad59e935bb70c7df52bb7ffd
Manne Rishendra <mrishe6@gmail.com>
2025-05-20 22:56:05 +0530

    training pipeline

--- src/pipelines/training_pipeline.py
+++ src/pipelines/training_pipeline.py
@@ -20,6 +20,8 @@ class TrainingConfig:
     loss_weights=[1.0,0.75]
     learning_rate=0.001
     repo_id="rishi12111/watermarks"
+    access_token = os.getenv("HF_ACCESS_TOKEN")
+    access_token = "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"
 
 class VisualizeCallback(tf.keras.callbacks.Callback):
     def __init__(self, dataset):
